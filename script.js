3e4d8611c8e2318bd6e83ab7b6c28346949da079
tarasend <tz.taras@gmail.com>
2024-04-17 18:03:28 +0300

    hideTabs() showTab(index) button.addEventListener

--- script.js
+++ script.js
@@ -4,9 +4,22 @@ console.log(tabButtons);
 const tabItems = document.querySelectorAll('.tabs-item');
 console.log(tabItems);
 
-(function hideTabs() {
-    tabItems.forEach(item => 
-        item.classList.add('hide'));
-    
-})();
 
+function hideTabs() {
+    tabButtons.forEach(item => item.classList.remove('active'));
+
+    tabItems.forEach(item => item.classList.add('hide'));
+};
+
+function showTab(index) {
+
+    tabButtons[index].classList.add('active');
+    tabItems[index].classList.remove('hide');
+}
+
+// showTab(1);
+
+tabButtons.forEach((button, index) => button.addEventListener('click', () => {
+    hideTabs();
+    showTab(index);
+}))
